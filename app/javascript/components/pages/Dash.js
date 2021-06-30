570e6feaf6811eda04b90ada9728d19188e04ece
Shazeen15 <shazzy.sterlin@gmail.com>
2021-06-30 09:42:18 -0700

    fixing img errors for landing page

--- app/javascript/components/pages/Dash.js
+++ app/javascript/components/pages/Dash.js
@@ -4,7 +4,7 @@ export default class Dash extends Component {
   render() {
     const { current_user } = this.props;
     return (
-      <div>
+      <div className="dash">
         <h2>Welcome {current_user.email}</h2>
       </div>
     );
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,3 @@ ActiveStorage.start();
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
-
-import "src/stylesheets/application";
-const images = require.context("../images", true);
